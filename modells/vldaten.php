17886cc7008c7024181434a78635b746faa5c898
Johannes <jstichler@gmail.com>
2013-09-05 18:56:30 +0200

    Lizenzinfo hinzugefügt Debug Meldungen entfernt

--- modells/vldaten.php
+++ modells/vldaten.php
@@ -1,6 +1,28 @@
 <?php
 /**
- * Liefert die Vorlesungsdaten
+ * Datei ist Teil neoEinrichtungstermine
+ * Erstellt von: johannes.stichler
+ * Datum: 01.08.13
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Johannes Stichler
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 class vldaten {
@@ -8,7 +30,6 @@ class vldaten {
 	function __construct()
 	{
 		$this->flash = Trails_Flash::instance();
-
 	}
 
 	function getAllVlsDay($day = false, $instid = false) {
@@ -25,7 +46,6 @@ class vldaten {
 					"date BETWEEN ".$day." AND ".$dayend. " ";
 					"ORDER BY date ".
 					"LIMIT 30";
-		//$this->flash->debug .= $sql."<br>";
 		$termine = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 		return $termine;
 	}
